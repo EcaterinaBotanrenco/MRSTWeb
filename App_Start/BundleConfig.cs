9555bd660db75916d56a00fba9e3a966d0e952ce
Catea26 <131901807+Catea26@users.noreply.github.com>
2024-02-23 16:01:09 +0200

    Initial commit

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebApplication1
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/localScripts").Include(
+                "~/assets/lib/easing/easing.min.js",
+                "~/assets/lib/waypoints/waypoints.min.js",
+                "~/assets/lib/counterup/counterup.min.js",
+                "~/assets/lib/owlcarousel/owl.carousel.min.js",
+                "~/assets/lib/tempusdominus/js/moment.min.js",
+                "~/assets/lib/tempusdominus/js/moment-timezone.min.js",
+                "~/assets/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js",
+                "~/assets/mail/jqBootstrapValidation.min.js",
+                "~/assets/mail/contact.js",
+                "~/assets/js/main.js"));
+
+            // Style bundle
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/bootstrap.min.css",
+                        "~/assets/lib/animate/animate.min.css",
+                        "~/assets/lib/owlcarousel/assets/owl.carousel.min.css",
+                        "~/assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css",
+                        "~/assets/css/style.css"));
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MRSUTW.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult about()
+        {
+            return View();
+        }
+        public ActionResult service()
+        {
+            return View();
+        }
+        public ActionResult price()
+        {
+            return View();
+        }
+
+        public ActionResult Appointment()
+        {
+            return View();
+        }
+        public ActionResult OpenHours()
+        {
+            return View();
+        }
+        public ActionResult OurTeam()
+        {
+            return View();
+        }
+        public ActionResult Testimonial()
+        {
+            return View();
+        }
+        public ActionResult contact()
+        {
+            return View();
+        }
+
+
+
+    }
+}
