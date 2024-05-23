6c6af5075d93ef7d9836650e605139ae4e9faec3
Catea26 <131901807+Catea26@users.noreply.github.com>
2024-05-23 10:58:35 +0300

    # Please enter the commit message for your changes. Lines starting

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -110,26 +110,7 @@ namespace WebApplication1.Controllers
 
             return View();
         }
-        public ActionResult Test()
-        {
-            SessionStatus();
-            if ((string)System.Web.HttpContext.Current.Session["LoginStatus"] != "login")
-            {
-                return RedirectToAction("Login", "Login");
-            }
-            
-            var user = System.Web.HttpContext.Current.GetMySessionObject();
-            UserData u = new UserData
-            {
-                Username = user.Username,
-                Level = user.Level,
-            };
-
-            ViewBag.username = u.Username;
-            ViewBag.userLevel = u.Level;
-
-            return View();
-        }
+        
         public ActionResult OpenHours()
         {
             SessionStatus();
